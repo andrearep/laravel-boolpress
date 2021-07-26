b82e267413b72e6dc5556bb85055b268ed064c3c
andrearep <andrea.repe@gmail.com>
2021-07-26 18:04:12 +0200

    creato modello migration seeder e collegati table post e category

--- app/Post.php
+++ app/Post.php
@@ -3,8 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     protected $fillable =['title', 'image', 'body'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
